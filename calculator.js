db6a1bb45973c8387ed3efee767d2f1564c07e4d
Lizz010 <lizzp0110@gmail.com>
2023-05-08 20:55:53 -0500

    Adding identifiers

---
+++ calculator.js
@@ -0,0 +1,7 @@
+const botonNumbers = document.getElementsByName('data-number');
+const botonOpera = document.getElementsByName('data-opera');
+const botonIgual = document.getElementsByName('data-igual');
+const botonDelete = document.getElementsByName('data-delete');
+var result = document.getElementById('result');
+
+console.log(botonNumbers);
